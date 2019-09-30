10ba0b7aac2b629a71127ebade2be5a8b165e956
ALEX DUTRA <alex.dutra@serpro.gov.br>
2019-09-30 15:35:53 -0300

    segundo commit

---
+++ src/main/java/com/carros/api/upload/UploadInput.java
@@ -0,0 +1,11 @@
+package com.carros.api.upload;
+
+import lombok.Data;
+
+@Data
+public class UploadInput {
+
+	private String fileName;
+	private String base64;
+	private String mimeType;
+}
---
+++ src/main/java/com/carros/api/upload/UploadOutput.java
@@ -0,0 +1,13 @@
+package com.carros.api.upload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadOutput {
+
+	private String url;
+}
